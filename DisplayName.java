2bfcf7250c3044503d6b0080c6b295c2ec8e06c2
Naresh Kumar H <nareshkumarh@live.com>
2021-04-13 17:13:13 +0530

    Add files via upload

---
+++ DisplayName.java
@@ -0,0 +1,17 @@
+package com.chainsys.marksapp;
+
+public class DisplayName {
+
+	// zoom meeting id: 9994194773 password:123
+	public static void naresh(String name) {
+
+		System.out.println("My name is " + name);
+	}
+
+	public static void main(String[] args) {
+		String name = "Naresh";
+		naresh(name); // Hi Naresh, Can you print the name which I have given ?
+
+	}
+
+}
