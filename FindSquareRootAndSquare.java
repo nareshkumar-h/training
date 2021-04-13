b21cc7780ee6cbb5f5bc7a658f773a6a1b4d0d2d
Naresh Kumar H <nareshkumarh@live.com>
2021-04-13 17:13:35 +0530

    Add files via upload

---
+++ FindSquareRootAndSquare.java
@@ -0,0 +1,28 @@
+package com.chainsys.marksapp;
+//9994194773- password:1234 - nareshkumarh@live.com
+class Mathh {
+	public static double sqrt(int a) {
+		double result = 0;
+		///
+		return result;
+	}
+}
+public class FindSquareRootAndSquare {
+
+	//Input: 2, Output: 4
+	public static int square(int a) {
+		int result = a*a;
+		return result;
+	}
+
+	public static void main(String[] args) {
+
+		int no = 9;
+		double result = square(9);//Math.sqrt(no);
+		System.out.println(result);
+		//output: 81
+				
+	}
+	
+
+}
